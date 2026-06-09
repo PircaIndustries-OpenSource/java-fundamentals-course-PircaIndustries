91d0f7bc9b58a6b09b2a4ace3b518aad621df11f
ManuelFT4 <jhonymanuelft@gmail.com>
2026-06-08 22:59:19 -0500

    Add logic to check Dragon Balls in InvocacionShenlong
    Implement main method to check Dragon Balls count.

--- starter-files/exercise-3a.java
+++ starter-files/exercise-3a.java
@@ -1,1 +1,23 @@
-
+public class InvocacionShenlong {
+    public static void main(String[] args) {
+        
+        // 1. Crea una variable de número (int) que guarde cuántas Esferas del Dragón tienes.
+        // Ponle un número del 1 al 7:
+        
+        
+        // 2. ¡Hora de comprobar! Usa un 'if' (si). 
+        // Pregunta si tus esferas son exactamente iguales ( == ) a 7.
+        
+        
+            // 3. Si tienes las 7, usa System.out.println para imprimir: 
+            // "¡Sal de ahí Shenlong y cumple mi deseo!"
+            
+            
+        // 4. Ahora usa un 'else' (si no pasa lo de arriba, o sea, tienes menos de 7). 
+        
+        
+            // 5. Adentro del else imprime: "Aún te faltan esferas. ¡Enciende el radar!"
+            
+            
+    }
+}
