d3d83908ded7856f117b70389598159229cd3f74
ManuelFT4 <jhonymanuelft@gmail.com>
2026-06-09 07:03:14 -0500

    Update example-3b.java

--- completed-examples/example-3b.java
+++ completed-examples/example-3b.java
@@ -1,1 +1,7 @@
-
+public class CombatDecision{
+  public static void main(String[] args){
+    for (int kame = 1; kame <= 10; kame++){
+      System.out.println("KameHameHa numero: " + kame);
+    }
+  }
+}
