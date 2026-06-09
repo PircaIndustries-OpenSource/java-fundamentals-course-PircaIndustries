41e4d3fa9a89549083e225a39abb167d5c0a6ea5
ManuelFT4 <jhonymanuelft@gmail.com>
2026-06-09 07:06:33 -0500

    Implement combat decision logic based on Ki levels

--- completed-examples/example-3a.java
+++ completed-examples/example-3a.java
@@ -1,1 +1,13 @@
+public class CombatDecision{
+  public static void main(String[] args){
+    int myKi = 5000;
+    //int enemyKi = 9000; /Para probar el primer escenario/
+    int enemyKi = 4000;
 
+    if(enemyKi>myKi){
+      System.out.println("Usar Kaio-Ken x10!");
+    } else {
+      System.out.println("Pelear en forma base");
+    }
+  }
+}
