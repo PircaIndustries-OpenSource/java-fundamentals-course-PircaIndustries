679338b376662c0f87b13c6184ea6fd0d2385e4b
tree_hugger <155770085+marc3lllob7@users.noreply.github.com>
2026-06-10 08:14:33 -0500

    feat: add exercise-4.java for POO introduction
    Inspired on My Hero Academia's franchise

---
+++ starter-files/exercise-4.java
@@ -0,0 +1,32 @@
+import java.util.Scanner; // Importamos la herramienta para leer texto por teclado
+
+// Creamos la clase Hero (El "molde" para diseñar héroes)
+    // Atributo para almacenar el nombre de héroe
+    // Un método o acción propia de nuestro héroe
+
+public class Main {
+    
+    // Método para dar la bienvenida (Código reutilizable)
+
+    public static void main(String[] args) {
+        
+        // Llamamos a nuestro método de saludo
+
+        // Configuración de Entrada y Salida con Scanner
+        
+        // Preguntamos por el nombre de estudiante
+        // Leemos el texto del teclado
+        
+        // Saludo personalizado
+
+        // Introducción a la POO: creando nuestro primer objeto
+        
+        // Instanciamos el objeto a partir de la clase
+        
+        // Eliges un nombre de héroe y lo ingresas
+        // Asignamos el valor al atributo
+        
+        // Indicamos el registro
+        // Invocamos la acción del objeto
+    }
+}
